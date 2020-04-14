ef1d64393bed3e918cc466bc3821c4d1c0e40faa
Yimu Yang <yimuyang19911223@gmail.com>
2020-04-13 23:18:05 -0700

    assignment 2

--- cmpe285_assignment2/finance/views.py
+++ cmpe285_assignment2/finance/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
+
+def calculate(request):
+    if request.method == 'POST':
+        stock_label = request.POST['stock_label']
+        time = 'current time'
+        company_name = 'company name'
+        stock_price = '+1'
+        value_changes = '+1'
+        percentage_changes = '+1%'
+
+        input = {'stock_label': stock_label, 'time': time, 'company_name': company_name, 'stock_price': stock_price, 'value_changes': value_changes, 'percentage_changes': percentage_changes}
+        return render(request, 'result.html', input)
